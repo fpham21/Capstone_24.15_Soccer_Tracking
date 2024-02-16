31014cc2d6813f6987a6e2e4988ad4b23cf1ba44
Michael Luna <136510705+Mikezilla02@users.noreply.github.com>
2024-02-16 11:10:36 -0800

    Update and rename main.py to borderDetection.py

--- borderDetection.py
+++ borderDetection.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Read the original image
-img = cv2.imread('21.jpg')
+img = cv2.imread('21.jpg')    #can automate this in the future
 # Display original image
 cv2.imshow('Original', img)
 cv2.waitKey(0)
