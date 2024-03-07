6e82a05657ed25d2d854b602464eb7fbb098414d
Michael Luna <136510705+Mikezilla02@users.noreply.github.com>
2024-03-07 11:05:24 -0800

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-image = cv2.imread('images/test3.png')
+image = cv2.imread('soccerField/3.jpg')
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
